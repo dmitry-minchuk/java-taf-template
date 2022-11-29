44900f4304b89644d442819917b7ed648a6dcd72
Dmitry Minchuk <dmitry.s.minchuk@gmail.com>
2022-11-29 16:21:52 +0100

    minor update

--- src/main/java/configuration/appcontainer/AppContainerFactory.java
+++ src/main/java/configuration/appcontainer/AppContainerFactory.java
@@ -11,7 +11,6 @@ import org.testcontainers.utility.DockerImageName;
 
 public class AppContainerFactory {
     protected static final Logger LOGGER = LogManager.getLogger(AppContainerFactory.class);
-    private final static String APP_HOST_URL_TEMPLATE = "http://%s:%s%s";
     private final static Integer APP_PORT = Integer.parseInt(ProjectConfiguration.getProperty(PropertyNameSpace.DEFAULT_APP_PORT));
     private final static String DEPLOYED_APP_PATH = ProjectConfiguration.getProperty(PropertyNameSpace.DEPLOYED_APP_PATH);
     private final static String DOCKER_IMAGE_NAME = ProjectConfiguration.getProperty(PropertyNameSpace.DOCKER_IMAGE_NAME);
@@ -25,10 +24,6 @@ public class AppContainerFactory {
         container.waitingFor(Wait.forHttp(DEPLOYED_APP_PATH));
         LOGGER.info(String.format("Application Localhost accessible url for %s: http://localhost:%s%s", containerName, container.getMappedPort(APP_PORT), DEPLOYED_APP_PATH));
         LOGGER.info(String.format("Application Url accessible from the Selenium container: http://%s:%s%s", containerName, APP_PORT, DEPLOYED_APP_PATH));
-        return new AppContainerData(container, String.format(
-                APP_HOST_URL_TEMPLATE,
-                containerName,
-                APP_PORT,
-                DEPLOYED_APP_PATH));
+        return new AppContainerData(container, String.format("http://%s:%s%s", containerName, APP_PORT, DEPLOYED_APP_PATH));
     }
 }
