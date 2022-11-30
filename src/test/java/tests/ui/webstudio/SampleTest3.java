a69f911906f613bc5790d12d503da1fe07ae4fc6
Dmitry Minchuk <dmitry.s.minchuk@gmail.com>
2022-11-30 14:24:10 +0100

    debugging

--- src/test/java/tests/ui/webstudio/SampleTest3.java
+++ src/test/java/tests/ui/webstudio/SampleTest3.java
@@ -10,6 +10,12 @@ public class SampleTest3 {
     @Test
     public void test() {
         AppContainerFactory.createContainer(StringUtil.generateUniqueName());
+        DriverFactory.getContainerizedDriver();
+        System.out.println();
+    }
+
+    @Test
+    public void test2() {
         AppContainerFactory.createContainer(StringUtil.generateUniqueName());
         DriverFactory.getContainerizedDriver();
         System.out.println();
