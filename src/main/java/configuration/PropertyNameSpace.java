5e76ee9ff425b20a03167e87186a8d2ed8c5731a
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-04 17:49:07 +0200

    DriverFactory improved

--- src/main/java/configuration/PropertyNameSpace.java
+++ src/main/java/configuration/PropertyNameSpace.java
@@ -2,6 +2,7 @@ package configuration;
 
 public enum PropertyNameSpace {
     BROWSER("browser"),
+    BROWSER_VERSION("browser_version"),
     RETRY_COUNT("retry_count"),
     DEFAULT_APP_PORT("default_app_port"),
     DOCKER_IMAGE_NAME("docker_image_name"),
