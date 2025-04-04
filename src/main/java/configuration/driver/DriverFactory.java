d725b668dc09fb452285e14cef4c0aade8c97343
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-04 17:49:07 +0200

    DriverFactory improved

--- src/main/java/configuration/driver/DriverFactory.java
+++ src/main/java/configuration/driver/DriverFactory.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
@@ -18,34 +16,23 @@ import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.logging.Level;
+import java.util.Map;
+import java.util.function.Function;
 
 public class DriverFactory {
     protected static final Logger LOGGER = LogManager.getLogger(DriverFactory.class);
-    private final static Integer VNC_PORT = 7900;
+    private static final Integer VNC_PORT = 7900;
+
+    private static final Map<String, Function<Network, ContainerizedDriver>> DRIVER_MAP = Map.of(
+            "chrome", network -> createContainerizedDriver(network, new ChromeOptions(), "selenium/standalone-chromium"),
+            "firefox", network -> createContainerizedDriver(network, new FirefoxOptions().setLogLevel(FirefoxDriverLogLevel.INFO), "selenium/standalone-firefox")
+    );
 
     public static ContainerizedDriver getContainerizedDriver(Network network) {
-        DriverEventLogger driverEventLogger = new DriverEventLogger();
         String driverName = ProjectConfiguration.getProperty(PropertyNameSpace.BROWSER);
-        ContainerizedDriver containerizedDriver;
-        switch (driverName) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                LoggingPreferences logs = new LoggingPreferences();
-                logs.enable(LogType.BROWSER, Level.ALL);
-                logs.enable(LogType.PERFORMANCE, Level.INFO);
-                chromeOptions.setCapability("goog:loggingPrefs", logs);
-                BrowserWebDriverContainer<?> chromeContainer = buildContainer("selenium/standalone-chromium:latest", chromeOptions, network);
-                containerizedDriver = new ContainerizedDriver(new EventFiringDecorator(driverEventLogger).decorate(new RemoteWebDriver(chromeContainer.getSeleniumAddress(), new ChromeOptions())), chromeContainer);
-                return containerizedDriver;
-            case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.setLogLevel(FirefoxDriverLogLevel.INFO);
-                BrowserWebDriverContainer<?> firefoxContainer = buildContainer("selenium/standalone-firefox:latest", firefoxOptions, network);
-                containerizedDriver = new ContainerizedDriver(new EventFiringDecorator(driverEventLogger).decorate(new RemoteWebDriver(firefoxContainer.getSeleniumAddress(), new FirefoxOptions())), firefoxContainer);
-                return containerizedDriver;
-        }
-        throw new RuntimeException("DriverName unknown: " + driverName);
+        return DRIVER_MAP.getOrDefault(driverName, n -> {
+            throw new RuntimeException("Unknown DriverName: " + driverName);
+        }).apply(network);
     }
 
     private static AbstractDriverOptions<?> addCommonBrowserOptions(AbstractDriverOptions<?> browserOptions) {
@@ -53,17 +40,22 @@ public class DriverFactory {
         return browserOptions;
     }
 
-    private static BrowserWebDriverContainer<?> buildContainer(String browserImageName, AbstractDriverOptions<?> browserOptions, Network network) {
-        DockerImageName dockerImageName = DockerImageName.parse(browserImageName)
-                .asCompatibleSubstituteFor("selenium/standalone-chrome");
-        BrowserWebDriverContainer<?> browserContainer = new BrowserWebDriverContainer<>(dockerImageName)
-                .withCapabilities(addCommonBrowserOptions(browserOptions));
-        browserContainer.withNetwork(network);
-        browserContainer.addExposedPort(VNC_PORT);
-        browserContainer.withEnv("SE_VNC_NO_PASSWORD", "1");
-        browserContainer.start();
-        LOGGER.info(String.format("Localhost accessible url for selenium VNC: http://localhost:%s", browserContainer.getMappedPort(VNC_PORT)));
-        LOGGER.info(String.format("Localhost accessible url for selenium HUB: %s", browserContainer.getSeleniumAddress()));
-        return browserContainer;
+    private static ContainerizedDriver createContainerizedDriver(Network network, AbstractDriverOptions<?> options, String baseImage) {
+        DriverEventLogger driverEventLogger = new DriverEventLogger();
+        String imageVersion = ProjectConfiguration.getProperty(PropertyNameSpace.BROWSER_VERSION);
+        DockerImageName dockerImageName = DockerImageName.parse(baseImage + ":" + imageVersion)
+                .asCompatibleSubstituteFor(baseImage.contains("chromium") ? "selenium/standalone-chrome" : baseImage);
+
+        BrowserWebDriverContainer<?> container = new BrowserWebDriverContainer<>(dockerImageName);
+        container.withCapabilities(addCommonBrowserOptions(options));
+        container.withNetwork(network);
+        container.addExposedPort(VNC_PORT);
+
+        container.start();
+        LOGGER.info("Localhost accessible URL for selenium VNC: http://localhost:{}", container.getMappedPort(VNC_PORT));
+        LOGGER.info("Localhost accessible URL for selenium HUB: {}", container.getSeleniumAddress());
+
+        EventFiringDecorator<RemoteWebDriver> decoratedDriver = new EventFiringDecorator<>(driverEventLogger);
+        return new ContainerizedDriver(decoratedDriver.decorate(new RemoteWebDriver(container.getSeleniumAddress(), options)), container);
     }
 }
