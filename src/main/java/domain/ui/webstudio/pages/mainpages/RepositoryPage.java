1fb0862ec6b3e3373f0100116b093af0f76fc847
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-17 15:43:04 +0200

    createProjectFromExcelFile() debug on Jenkins : WaitUtil.sleep(250000);

--- src/main/java/domain/ui/webstudio/pages/mainpages/RepositoryPage.java
+++ src/main/java/domain/ui/webstudio/pages/mainpages/RepositoryPage.java
@@ -37,7 +37,7 @@ public class RepositoryPage extends ProxyMainPage {
         excelFilesComponent.createProjectFromExcelFile(fileName, projectName);
         if(configureCommitInfoComponent.isPresent())
             configureCommitInfoComponent.fillCommitInfoWithRandomData();
-        WaitUtil.sleep(250);
+        WaitUtil.sleep(250000);
         refreshBtn.click();
     }
 
