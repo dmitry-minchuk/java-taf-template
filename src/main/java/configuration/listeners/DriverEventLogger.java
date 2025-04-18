7d11def192541ec07a88c6f78f460b21d7857f36
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-18 14:47:59 +0200

    logs and file attachment added

--- src/main/java/configuration/listeners/DriverEventLogger.java
+++ src/main/java/configuration/listeners/DriverEventLogger.java
@@ -1,5 +1,6 @@
 package configuration.listeners;
 
+import com.epam.reportportal.service.ReportPortal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Alert;
@@ -8,53 +9,74 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class DriverEventLogger implements WebDriverListener {
     protected static final Logger LOGGER = LogManager.getLogger(DriverEventLogger.class);
 
     @Override
     public void beforeGet(WebDriver driver, String url) {
-        LOGGER.info(String.format("Opening url: %s", url));
+        String logMessage = String.format("Opening url: %s", url);
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeClick(WebElement element) {
-        LOGGER.info(String.format("Click on element: '%s'", getElementIdentifier(element)));
+        String logMessage = String.format("Click on element: '%s'", getElementIdentifier(element));
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeSendKeys(WebElement element, CharSequence... keysToSend) {
-        LOGGER.info(String.format("Type text '%s' into '%s'", Arrays.toString(keysToSend), getElementIdentifier(element)));
+        String logMessage = String.format("Type text '%s' into '%s'", Arrays.toString(keysToSend), getElementIdentifier(element));
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeClear(WebElement element) {
-        LOGGER.info(String.format("Clear '%s'", getElementIdentifier(element)));
+        String logMessage = String.format("Clear '%s'", getElementIdentifier(element));
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeAccept(Alert alert) {
-        LOGGER.info(String.format("Accept alert '%s'", alert.getText()));
+        String logMessage = String.format("Accept alert '%s'", alert.getText());
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeMaximize(WebDriver.Window window) {
-        LOGGER.info(String.format("Maximize window to height:'%s' width:'%s'", window.getSize().height, window.getSize().width));
+        String logMessage = String.format("Maximize window to height:'%s' width:'%s'", window.getSize().height, window.getSize().width);
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeQuit(WebDriver driver) {
-        LOGGER.info("Closing browser");
+        String logMessage = "Closing browser";
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     @Override
     public void beforeClose(WebDriver driver) {
-        LOGGER.info(String.format("Closing browser tab '%s' with title '%s'", driver.getWindowHandle(), driver.getTitle()));
+        String logMessage = String.format("Closing browser tab '%s' with title '%s'", driver.getWindowHandle(), driver.getTitle());
+        LOGGER.info(logMessage);
+        reportPortalLogger(logMessage);
     }
 
     private String getElementIdentifier(WebElement element) {
         if(!element.getAccessibleName().isBlank() && !element.getAccessibleName().isBlank())
             return element.getAccessibleName();
         return element.getTagName();
+    }
+
+    private void reportPortalLogger(String logMessage) {
+        ReportPortal.emitLog(logMessage, "INFO", new Date());
     }
 }
--- src/main/java/configuration/projectconfig/PropertyNameSpace.java
+++ src/main/java/configuration/projectconfig/PropertyNameSpace.java
@@ -8,8 +8,10 @@ public enum PropertyNameSpace {
     DEFAULT_APP_PORT("default_app_port"),
     DOCKER_IMAGE_NAME("docker_image_name"),
     DEPLOYED_APP_PATH("deployed_app_path"),
-    HOST_RESOURCE_PATH("hostResourcePath"),
-    CONTAINER_RESOURCE_PATH("containerResourcePath"),
+    HOST_RESOURCE_PATH("host_resource_path"),
+    CONTAINER_RESOURCE_PATH("container_resource_path"),
+    HOST_SCREENSHOTS_PATH("host_screenshot_path"),
+    CONTAINER_SCREENSHOTS_PATH("container_screenshot_path"),
     GIT_URL_RULESERVICE("git.url.ruleservice"),
     GIT_LOGIN_RULESERVICE("git.login.ruleservice"),
     GIT_TOKEN_RULESERVICE("git.token.ruleservice");
