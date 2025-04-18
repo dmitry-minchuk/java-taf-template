dd9f38bb56a8533555d01ac9fc2285c505b9dadd
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-18 15:29:48 +0200

    Revert "ScreenShotUtil permissions added"
    This reverts commit ee82ff64edf75d2f661390ec854574ee4b4c1fe2.

--- src/main/java/helpers/utils/ScreenShotUtil.java
+++ src/main/java/helpers/utils/ScreenShotUtil.java
@@ -22,10 +22,9 @@ public class ScreenShotUtil {
             TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
             File screenshotFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
             File destinationDir = new File(HOST_SCREENSHOT_RELATIVE_PATH);
-            ensureScreenshotDirectoryExistsAndHasPermissions(destinationDir.getAbsolutePath());
-//            if (!destinationDir.exists()) {
-//                destinationDir.mkdirs();
-//            }
+            if (!destinationDir.exists()) {
+                destinationDir.mkdirs();
+            }
             File destinationFile = new File(destinationDir, screenshotName);
             try {
                 FileUtils.copyFile(screenshotFile, destinationFile);
@@ -38,29 +37,6 @@ public class ScreenShotUtil {
         } else {
             LOGGER.warn("Driver cannot make screenshot at the moment.");
             return null;
-        }
-    }
-
-    private static void ensureScreenshotDirectoryExistsAndHasPermissions(String directoryPath) {
-        File directory = new File(directoryPath);
-        if (!directory.exists()) {
-            if (directory.mkdirs()) {
-                LOGGER.info("Директория для скриншотов создана: {}", directory.getAbsolutePath());
-                try {
-                    ProcessBuilder pb = new ProcessBuilder("chmod", "-R", "775", directory.getAbsolutePath());
-                    Process process = pb.start();
-                    int exitCode = process.waitFor();
-                    if (exitCode == 0) {
-                        LOGGER.info("Permissions set.");
-                    } else {
-                        LOGGER.warn("Could not set permissions. (Exit code: {}).", exitCode);
-                    }
-                } catch (IOException | InterruptedException e) {
-                    LOGGER.warn("Error while setting permissions for screenshot directory: {}", e.getMessage());
-                }
-            } else {
-                LOGGER.error("Could not create screenshot directory: {}", directory.getAbsolutePath());
-            }
         }
     }
 
