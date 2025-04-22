ff906f9cefd6d7f597e7534fd81350ad0bf9e235
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-22 12:53:39 +0200

    jira ticket added to attributes

--- src/main/java/configuration/listeners/JiraTicketListener.java
+++ src/main/java/configuration/listeners/JiraTicketListener.java
@@ -5,8 +5,12 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JiraTicketListener implements ITestListener {
+
+    private static final String JIRA_BASE_URL = "https://jira.eisgroup.com/browse/";
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -14,7 +18,19 @@ public class JiraTicketListener implements ITestListener {
         if (method.isAnnotationPresent(JiraTicket.class)) {
             JiraTicket jiraTicket = method.getAnnotation(JiraTicket.class);
             String ticketId = jiraTicket.value();
-            // here ticketId can be passed into Report Portal or other reporting tool
+
+            Object existing = result.getAttribute("rp.attributes");
+
+            Map<String, String> attributes;
+            if (existing instanceof Map) {
+                attributes = new HashMap<>((Map<String, String>) existing);
+            } else {
+                attributes = new HashMap<>();
+            }
+
+            String jiraLink = JIRA_BASE_URL + ticketId;
+            attributes.put("jira", jiraLink);
+            result.setAttribute("rp.attributes", attributes);
         }
     }
 }
