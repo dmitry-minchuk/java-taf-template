be7a79626b07dab0fbec5060ae6424352e5d17eb
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-04-22 11:34:41 +0200

    ScreenShotUtil updated not to use container at all

--- src/main/java/helpers/utils/ScreenShotUtil.java
+++ src/main/java/helpers/utils/ScreenShotUtil.java
@@ -3,13 +3,13 @@ package helpers.utils;
 import configuration.driver.ContainerizedDriver;
 import configuration.projectconfig.ProjectConfiguration;
 import configuration.projectconfig.PropertyNameSpace;
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -17,35 +17,24 @@ public class ScreenShotUtil {
 
     private static final Logger LOGGER = LogManager.getLogger(ScreenShotUtil.class);
     private final static String HOST_SCREENSHOT_RELATIVE_PATH = ProjectConfiguration.getProperty(PropertyNameSpace.HOST_SCREENSHOTS_PATH);
-    private final static String CONTAINER_SCREENSHOT_PATH = ProjectConfiguration.getProperty(PropertyNameSpace.CONTAINER_SCREENSHOTS_PATH);
 
     public static File takeAndSaveScreenshot(ContainerizedDriver containerizedDriver) {
         if (containerizedDriver.getDriver() instanceof TakesScreenshot takesScreenshot) {
-            File screenshotFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-            File destinationDirInContainer = new File(CONTAINER_SCREENSHOT_PATH);
+            byte[] screenshotBytes = takesScreenshot.getScreenshotAs(OutputType.BYTES);
             String screenshotName = StringUtil.generateUniqueName("failed-test") + ".png";
-            File destinationFileInContainer = new File(destinationDirInContainer, screenshotName);
+            File hostDir = new File(HOST_SCREENSHOT_RELATIVE_PATH);
+            File hostFile = new File(hostDir, screenshotName);
 
-            if (!destinationDirInContainer.exists()) {
-                destinationDirInContainer.mkdirs();
+            if (!hostDir.exists()) {
+                hostDir.mkdirs();
             }
 
-            try {
-                FileUtils.copyFile(screenshotFile, destinationFileInContainer);
-                LOGGER.info("Screenshot saved into container: {}", destinationFileInContainer.getAbsolutePath());
-
-                File hostDir = new File(HOST_SCREENSHOT_RELATIVE_PATH);
-                if (!hostDir.exists()) {
-                    hostDir.mkdirs();
-                }
-                File hostFile = new File(hostDir, screenshotName);
-                containerizedDriver.getDriverContainer().copyFileFromContainer(destinationFileInContainer.getAbsolutePath(), hostFile.getAbsolutePath());
-                LOGGER.info("Screenshot copied to host: {}", hostFile.getAbsolutePath());
-
+            try (FileOutputStream fos = new FileOutputStream(hostFile)) {
+                fos.write(screenshotBytes);
+                LOGGER.info("Screenshot saved to host: {}", hostFile.getAbsolutePath());
                 return hostFile;
-
             } catch (IOException e) {
-                LOGGER.error("Error while saving/copying screenshot: {}", e.getMessage());
+                LOGGER.error("Error while saving screenshot to host: {}", e.getMessage());
                 LOGGER.error(PrintUtil.prettyPrintObjectCollection.apply(Arrays.stream(e.getStackTrace()).toList()));
                 return null;
             }
