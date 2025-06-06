20d73188c5eb3e5130bb309ad94449885d65f0e0
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-06-06 11:03:47 +0200

    EPBDS-13911: testTraceIntoFileJsonRequest added

--- src/main/java/configuration/core/ui/BasePage.java
+++ src/main/java/configuration/core/ui/BasePage.java
@@ -2,8 +2,10 @@ package configuration.core.ui;
 
 import configuration.appcontainer.AppContainerPool;
 import configuration.driver.DriverPool;
+import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.Objects;
 
@@ -11,6 +13,10 @@ public abstract class BasePage {
     protected static final Logger LOGGER = LogManager.getLogger(BasePage.class);
     protected String absoluteUrl = null;
     protected String urlAppender = "";
+
+    @Getter
+    @FindBy(xpath = "//*[contains(text(), '%s')]")
+    protected SmartWebElement universalTextElement;
 
     public BasePage() {
         LOGGER.info(this.getClass().getName() + " was opened.");
