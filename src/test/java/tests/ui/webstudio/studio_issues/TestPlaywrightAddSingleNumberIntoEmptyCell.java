197141ce01e033c945ddfa09a9f0bbf849f4c43a
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-08-26 16:29:13 +0200

    testPlaywrightAddSingleNumberIntoEmptyCell migrated

---
+++ src/test/java/tests/ui/webstudio/studio_issues/TestPlaywrightAddSingleNumberIntoEmptyCell.java
@@ -0,0 +1,41 @@
+package tests.ui.webstudio.studio_issues;
+
+import com.epam.reportportal.annotations.Description;
+import com.epam.reportportal.annotations.TestCaseId;
+import configuration.annotations.AppContainerConfig;
+import configuration.appcontainer.AppContainerStartParameters;
+import configuration.core.ui.PlaywrightTableComponent;
+import domain.serviceclasses.constants.User;
+import domain.ui.webstudio.components.editortabcomponents.PlaywrightEditTablePanelComponent;
+import domain.ui.webstudio.components.editortabcomponents.leftmenu.PlaywrightLeftRulesTreeComponent;
+import domain.ui.webstudio.pages.mainpages.PlaywrightEditorPage;
+import helpers.service.PlaywrightWorkflowService;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+import static domain.ui.webstudio.components.CreateNewProjectComponent.TabName.TEMPLATE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestPlaywrightAddSingleNumberIntoEmptyCell extends BaseTest {
+
+    @Test
+    @TestCaseId("EPBDS-7232")
+    @Description("BUG: Impossible to add single number into empty cell")
+    @AppContainerConfig(startParams = AppContainerStartParameters.DEFAULT_STUDIO_PARAMS)
+    public void testPlaywrightAddSingleNumberIntoEmptyCell() {
+        String projectName = PlaywrightWorkflowService.loginCreateProjectOpenEditor(User.ADMIN, TEMPLATE, "Example 1 - Bank Rating");
+        PlaywrightEditorPage editorPage = new PlaywrightEditorPage();
+        editorPage.getLeftProjectModuleSelectorComponent().selectModule(projectName, "Bank Rating");
+        editorPage.getLeftRulesTreeComponent()
+                .setViewFilter(PlaywrightLeftRulesTreeComponent.FilterOptions.BY_TYPE)
+                .expandFolderInTree("Decision")
+                .selectItemInFolder("Decision", "BankLimitIndex");
+        editorPage.getTableToolbarPanelComponent().getEditBtn().click();
+        PlaywrightEditTablePanelComponent editTablePanel = editorPage.getEditTablePanelComponent();
+        PlaywrightTableComponent tableComponent = editorPage.getCenterTable();
+        tableComponent.clickCell(10, 1);
+        editTablePanel.getInsertRowAfterBtn().click();
+        tableComponent.doubleClickAndPasteTextToCell(11, 1, "13", true);
+        assertThat(editorPage.getCenterTable().getCellText(11, 1)).isEqualTo("13");
+    }
+}
