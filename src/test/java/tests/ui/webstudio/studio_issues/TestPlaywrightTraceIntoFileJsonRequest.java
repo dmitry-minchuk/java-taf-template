e13108efb915f7b6150c01b621028258115e6fbb
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-08-27 13:20:41 +0200

    TestPlaywrightTraceIntoFileJsonRequest migrated

--- src/test/java/tests/ui/webstudio/studio_issues/TestPlaywrightTraceIntoFileJsonRequest.java
+++ src/test/java/tests/ui/webstudio/studio_issues/TestPlaywrightTraceIntoFileJsonRequest.java
@@ -5,27 +5,26 @@ import com.epam.reportportal.annotations.TestCaseId;
 import configuration.annotations.AppContainerConfig;
 import configuration.appcontainer.AppContainerStartParameters;
 import domain.serviceclasses.constants.User;
-import domain.ui.webstudio.components.editortabcomponents.leftmenu.LeftRulesTreeComponent;
-import domain.ui.webstudio.pages.mainpages.EditorPage;
-import helpers.service.WorkflowService;
+import domain.ui.webstudio.components.editortabcomponents.leftmenu.PlaywrightLeftRulesTreeComponent;
+import domain.ui.webstudio.pages.mainpages.PlaywrightEditorPage;
+import helpers.service.PlaywrightWorkflowService;
 import org.testng.annotations.Test;
 import tests.BaseTest;
 
-import static domain.ui.webstudio.components.CreateNewProjectComponent.TabName.ZIP_ARCHIVE;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestTraceIntoFileJsonRequest extends BaseTest {
+public class TestPlaywrightTraceIntoFileJsonRequest extends BaseTest {
 
     @Test
     @TestCaseId("EPBDS-8215")
     @Description("BUG: Internal Server error appears if user entered JSON request in 'Trace into File'")
     @AppContainerConfig(startParams = AppContainerStartParameters.DEFAULT_STUDIO_PARAMS)
-    public void testTraceIntoFileJsonRequest() {
-        String projectName = WorkflowService.loginCreateProject(User.ADMIN, ZIP_ARCHIVE, "testTraceIntoFileJsonRequest.zip");
-        EditorPage editorPage = new EditorPage();
+    public void testPlaywrightTraceIntoFileJsonRequest() {
+        String projectName = PlaywrightWorkflowService.loginCreateProjectFromZip(User.ADMIN, "testTraceIntoFileJsonRequest.zip");
+        PlaywrightEditorPage editorPage = new PlaywrightEditorPage();
         editorPage.getLeftProjectModuleSelectorComponent().selectModule(projectName, "the_nulls_input_parameters");
         editorPage.getLeftRulesTreeComponent()
-                .setViewFilter(LeftRulesTreeComponent.FilterOptions.BY_EXCEL_SHEET)
+                .setViewFilter(PlaywrightLeftRulesTreeComponent.FilterOptions.BY_EXCEL_SHEET)
                 .expandFolderInTree("Sheet1")
                 .selectItemInFolder("Sheet1", "mySpreadsheet");
 
@@ -34,7 +33,7 @@ public class TestTraceIntoFileJsonRequest extends BaseTest {
                 .selectJSONTrace("{\n  \"myId\": \"string\",\n  \"someArr\": null\n  \"intA\": 0,\n  \"subElement\": null\n  \"someText\": null\n}")
                 .clickTraceIntoFile();
 
-        assertThat(editorPage.getUniversalTextElement().format("Internal Server Error").isDisplayed(5))
+        assertThat(editorPage.locator("text=Internal Server Error").isVisible())
                 .as("Internal Server Error is shown!")
                 .isFalse();
     }
