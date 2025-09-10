4c67c87482196c871e4f79375efb2826c857f2cf
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-09-10 16:20:23 +0200

    minor updates

--- src/main/java/helpers/service/LoginService.java
+++ src/main/java/helpers/service/LoginService.java
@@ -3,15 +3,10 @@ package helpers.service;
 import com.microsoft.playwright.Page;
 import configuration.driver.LocalDriverPool;
 import domain.serviceclasses.models.UserData;
-import domain.ui.webstudio.pages.mainpages.AdminPage;
 import domain.ui.webstudio.pages.mainpages.EditorPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Playwright version of LoginService for user authentication
- * Provides login functionality using Playwright page automation
- */
 public class LoginService {
     
     protected static final Logger LOGGER = LogManager.getLogger(LoginService.class);
@@ -36,9 +31,7 @@ public class LoginService {
         
         usernameField.waitFor();
         usernameField.fill(user.getLogin());
-        
         passwordField.fill(user.getPassword());
-        
         loginButton.click();
         
         // Wait for successful login - editor page should load
@@ -46,31 +39,5 @@ public class LoginService {
         
         LOGGER.info("Successfully logged in as: {}", user.getLogin());
         return new EditorPage();
-    }
-    
-    // Navigate to Administration via User Menu: user-logo span → ant-drawer → Administration → Admin Page
-    public AdminPage navigateToAdministration() {
-        LOGGER.info("Navigating to Administration via User Menu");
-        
-        // Step 1: Click the user logo span to open the user menu
-        // Original Selenium: //div[@class='user-logo']/span
-        var userLogo = page.locator("div.user-logo span");
-        
-        userLogo.waitFor();
-        userLogo.click();
-        LOGGER.info("User logo clicked successfully");
-        
-        var userMenuDrawer = page.locator("div.ant-drawer-content-wrapper");
-        userMenuDrawer.waitFor();
-        LOGGER.info("User menu drawer opened successfully");
-        
-        var administrationMenuItem = page.locator("li.ant-menu-item:has(span:text('Administration'))");
-        administrationMenuItem.waitFor();
-        administrationMenuItem.click();
-        LOGGER.info("Administration menu item clicked");
-        
-        page.locator("div#main-menu, [data-menu-id]").first().waitFor();
-        
-        return new AdminPage();
     }
 }
