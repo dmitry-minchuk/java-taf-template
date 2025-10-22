5f37a3a60c3b4666313288e0cabefaf29c0e5134
dmitryminchuk <dmitry.s.minchuk@gmail.com>
2025-10-22 12:20:05 +0200

    RepositoryContentTabPropertiesComponent cleaned

--- src/main/java/domain/ui/webstudio/components/repositorytabcomponents/RepositoryContentTabPropertiesComponent.java
+++ src/main/java/domain/ui/webstudio/components/repositorytabcomponents/RepositoryContentTabPropertiesComponent.java
@@ -18,11 +18,6 @@ public class RepositoryContentTabPropertiesComponent extends BaseComponent {
     private static final Logger LOGGER = LogManager.getLogger(RepositoryContentTabPropertiesComponent.class);
 
     private TableComponent propertiesTable;
-    private WebElement authorElementTemplate;
-    private WebElement expandableRevision;
-    private WebElement tagSelectOpenTemplate;
-    private WebElement tagError;
-    private WebElement tagsSection;
 
     public RepositoryContentTabPropertiesComponent() {
         super(LocalDriverPool.getPage());
@@ -36,11 +31,6 @@ public class RepositoryContentTabPropertiesComponent extends BaseComponent {
 
     private void initializeElements() {
         propertiesTable = createScopedComponent(TableComponent.class, "xpath=.//table[@class='formfields']", "propertiesTable");
-        authorElementTemplate = createScopedElement("xpath=.//span[text()='%s']", "authorElementTemplate");
-        expandableRevision = createScopedElement("xpath=.//div[@class='rf-tab']//table[@class='formfields']//span[@class='expandable']", "expandableRevision");
-        tagSelectOpenTemplate = createScopedElement("xpath=.//h3[contains(text(), 'Tags')]/..//td[contains(text(), '%s')]/..//span", "tagSelectOpenTemplate");
-        tagError = createScopedElement("xpath=.//span[@class='error']", "tagError");
-        tagsSection = createScopedElement("xpath=.//h3[contains(text(), 'Tags')]", "tagsSection");
     }
 
     @Getter
@@ -67,83 +57,9 @@ public class RepositoryContentTabPropertiesComponent extends BaseComponent {
         }
     }
 
-    public static String[] properties = {
-            "Effective Date",
-            "Expiration Date",
-            "Start Request Date",
-            "End Request Date",
-            "Canada Region",
-            "Canada Province",
-            "Countries",
-            "Region",
-            "Currency",
-            "Language",
-            "LOB",
-            "Origin",
-            "US Region",
-            "US States",
-    };
-
     public String getProperty(Property name) {
         propertiesTable.isVisible();
         return propertiesTable.getCell(findRowByText(name.text), 2).getText().trim().replaceAll("\n", "");
-    }
-
-    public String getAuthorEmail(String author) {
-        return authorElementTemplate.format(author).getAttribute("title").trim().replaceAll("\n", "");
-    }
-
-    public void expandRevisionID() {
-        int revisionRow = findRowByText(Property.REVISION.text);
-        WebElement revisionCell = propertiesTable.getCell(revisionRow, 1);
-        WebElement expandableSpan = new WebElement(page,
-            "xpath=.//span[@class='expandable']", "expandableSpan");
-        expandableSpan.click();
-    }
-
-    public boolean isRevisionExpandable() {
-        return expandableRevision.isVisible(100);
-    }
-
-    public boolean isPropertyPresent(String nameProperty) {
-        try {
-            findRowByText(nameProperty);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public void checkPropertyPresent(String nameProperty, boolean isExpected) {
-        assertThat(isPropertyPresent(nameProperty))
-            .as("Property '" + nameProperty + "' presence:")
-            .isEqualTo(isExpected);
-    }
-
-    public void checkProperty(Property name, String value) {
-        if (!value.isEmpty()) {
-            if (value.toLowerCase().equals("today")) {
-                // Note: Extensions.getCurrentDate would need to be migrated or replaced
-                // value = Extensions.getCurrentDate("MM/dd/yyyy");
-            }
-            assertThat(getProperty(name)).as("Property '" + name + "':").isEqualTo(value);
-        }
-    }
-
-    public void checkMainProperties(HashMap<String, String> properties) {
-        Arrays.asList("Name:", "Status:", "Modified By:").forEach(property -> {
-            int row = findRowByText(property);
-            String actualValue = propertiesTable.getCell(row, 1).getText();
-            assertThat(actualValue).as("Property '" + property + "'").isEqualTo(properties.get(property));
-        });
-    }
-
-    public String getTagError() {
-        return tagError.getText();
-    }
-
-    public boolean isTagsSectionPresent() {
-        return tagsSection.isVisible(100);
     }
 
     // Helper method to find row index by text content (returns 1-based index)
